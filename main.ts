a97bf2ed8aad2ef3fa891c0af1fd47e47732b984
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-06 13:27:54 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,62 @@
  * This program will show all colors
 */
 
+basic.showIcon(IconNames.Happy)
 
+// button A
+input.onButtonPressed(Button.A, function () {
+
+  basic.clearScreen()
+
+  // turn on red
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  basic.showString("Red")
+  basic.pause(1000)
+
+  // turn on blue
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  basic.showString("Blue")
+  basic.pause(1000)
+
+  // turn on green
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("Green")
+  basic.pause(1000)
+
+  // turn on magenta
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  basic.showString("Magenta")
+  basic.pause(1000)
+
+  // turn on yellow
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("Yellow")
+  basic.pause(1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
