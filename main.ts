d49e6adfa1231274c8d4662bd4f5ad07c877bf5e
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-09 12:52:08 -0400

    7YUY

--- main.ts
+++ main.ts
@@ -13,54 +13,58 @@ input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
 
   // turn on red
-  pins.digitalWritePin(DigitalPin.P13, 0)
-  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 0)
   pins.digitalWritePin(DigitalPin.P15, 0)
   basic.showString("Red")
   basic.pause(1000)
 
   // turn on blue
-  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P13, 0)
   pins.digitalWritePin(DigitalPin.P14, 0)
-  pins.digitalWritePin(DigitalPin.P15, 0)
+  pins.digitalWritePin(DigitalPin.P15, 1)
   basic.showString("Blue")
   basic.pause(1000)
 
   // turn on green
   pins.digitalWritePin(DigitalPin.P13, 0)
-  pins.digitalWritePin(DigitalPin.P14, 0)
-  pins.digitalWritePin(DigitalPin.P15, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 0)
   basic.showString("Green")
   basic.pause(1000)
 
   // turn on magenta
   pins.digitalWritePin(DigitalPin.P13, 1)
-  pins.digitalWritePin(DigitalPin.P14, 1)
-  pins.digitalWritePin(DigitalPin.P15, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 1)
   basic.showString("Magenta")
   basic.pause(1000)
 
   // turn on yellow
-  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P13, 1)
   pins.digitalWritePin(DigitalPin.P14, 1)
-  pins.digitalWritePin(DigitalPin.P15, 1)
+  pins.digitalWritePin(DigitalPin.P15, 0)
   basic.showString("Yellow")
   basic.pause(1000)
 
+  // turn on cyan
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("Cyan")
+  basic.pause(1000)
 
+  // turn on white
+  pins.digitalWritePin(DigitalPin.P13, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString("White")
+  basic.pause(1000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // turn off
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0) 
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  basic.pause(1000)
 
 })
